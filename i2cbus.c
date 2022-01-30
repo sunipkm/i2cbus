3ea6589d3232bccb406bac7450dc4705fb4f8d89
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2022-01-30 18:28:49 -0500

    Simplified implementation. Breaking changes implemented with removal of broken context-based locking system.

--- i2cbus.c
+++ i2cbus.c
@@ -10,84 +10,126 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <pthread.h>
-#define __I2CBUS_INTERNAL
 #include "i2cbus.h"
-#undef __I2CBUS_INTERNAL
 
-#define eprintf(...) fprintf(stderr, __VA_ARGS__)
-static int __i2clock_initd = 0; /// Indicate that the I2C bus has not been initialized
+#define I2C_DEBUG
+
+#ifdef eprintf
+#undef eprintf
+#endif
+
+#define eprintf(str, ...) \
+    fprintf(stderr, "[%s/%s():%d] " str "\n", __FILE__, __func__, __LINE__, ##__VA_ARGS__)
+
+static int i2clock_initd = 0; /// Indicate that the I2C bus has not been initialized
+
+#ifndef I2CBUS_MAX_NUM
+#define I2CBUS_MAX_NUM 2 /// Maximum 2 /dev/i2cX
+#endif
+/**
+ * @brief Set of mutexes for the I2C bus
+ * 
+ */
+pthread_mutex_t i2cbus_locks[I2CBUS_MAX_NUM];
 
 int i2cbus_open(i2cbus *dev, int id, int addr)
 {
-    if (__i2clock_initd++ == 0) // only do it when the lock init is zero
+    int ret = 0;
+    char fname[256];
+    if (i2clock_initd++ == 0) // only do it when the lock init is zero
     {
-        for (int i = 0; i < __I2CBUS_MAX_NUM; i++)
+        pthread_mutexattr_t attr;
+        ret = pthread_mutexattr_init(&attr);
+        if (ret)
         {
-            int ret = pthread_mutex_init(&(__i2cbus_locks[i]), NULL);
-            if (ret != 0)
+            eprintf("Could not initialize mutex attribute");
+            ret = -1;
+            goto err;
+        }
+        ret = pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+        if (ret)
+        {
+            eprintf("Could not initialize mutex attribute");
+            pthread_mutexattr_destroy(&attr);
+            ret = -1;
+            goto err;
+        }
+        for (int i = 0; i < I2CBUS_MAX_NUM; i++)
+        {
+            ret = pthread_mutex_init(&(i2cbus_locks[i]), &attr);
+            if (ret)
             {
-                eprintf("%s: Failed to init mutex %d, ", __func__, i);
+                eprintf("Failed to init mutex %d, ", i);
                 perror("mutex init");
-                return -1;
+                pthread_mutexattr_destroy(&attr);
+                ret = -2;
+                goto err;
             }
         }
+        pthread_mutexattr_destroy(&attr);
     }
     // check 1: memory
     if (dev == NULL)
     {
-        fprintf(stderr, "%s: Error: Device descriptor is NULL\n", __func__);
-        return -1;
+        eprintf("Error: Device descriptor is NULL");
+        ret = -3;
+        goto err;
     }
-    // check 2: id > __I2CBUS_MAX_NUM - 1
-    if (!(id < __I2CBUS_MAX_NUM))
+    // check 2: id > I2CBUS_MAX_NUM - 1
+    if (!(id < I2CBUS_MAX_NUM))
     {
-        fprintf(stderr, "%s: System supports up to %d ID, you provided %d.\n", __func__, __I2CBUS_MAX_NUM - 1, id);
-        return -1;
+        eprintf("System supports up to %d ID, you provided %d.", I2CBUS_MAX_NUM - 1, id);
+        ret = -4;
+        goto err;
     }
     // check 3: addr valid range
     if (addr < 8)
     {
         fprintf(stderr, "%s: Address 0x%02x is invalid\n", __func__, addr);
-        return -1;
+        ret = -5;
+        goto err;
     }
 
     // Try to open the file descriptor
     // step 1: Create file name
-    char fname[256];
     if (snprintf(fname, 256, "/dev/i2c-%d", id) < 0)
     {
-        fprintf(stderr, "%s: Failed to generate device filename using snprintf. FATAL Error!\n", __func__);
-        return -1;
+        eprintf("Failed to generate device filename using snprintf. FATAL Error!");
+        ret = -6;
+        goto err;
     }
     if ((dev->fd = open(fname, O_RDWR)) < 0)
     {
-        fprintf(stderr, "%s: Failed to open %s. Error %d\n", __func__, fname, dev->fd);
-        return dev->fd;
+        eprintf("Failed to open %s. Error %d\n", fname, errno);
+        ret = -errno;
+        goto err;
     }
-    int status = 0;
-    if ((status = ioctl(dev->fd, I2C_SLAVE, addr)) < 0)
+    if (ioctl(dev->fd, I2C_SLAVE, addr) < 0)
     {
-        fprintf(stderr, "%s: Failed to open I2C slave address 0x%02x on bus %s with error %d, returning...\n", __func__, addr, fname, status);
+        eprintf("Failed to open I2C slave address 0x%02x on bus %s with error %d, returning...", addr, fname, errno);
         close(dev->fd);
-        return status;
+        ret = -errno;
+        goto err;
     }
     // if we are here, then everything was successful
-    dev->id = id;                      // assign device id
-    dev->lock = &(__i2cbus_locks[id]); // assign lock
-    dev->ctx = -1;                     // context has been cleared, it has to be set by the caller
+    dev->id = id;                    // assign device id
+    dev->lock = &(i2cbus_locks[id]); // assign lock
     return dev->fd;
+err:
+    i2clock_initd--;
+    return -1;
 }
 
 int i2cbus_close(i2cbus *dev)
 {
-    if (--__i2clock_initd == 0) // only do it when the lock init is zero
+    if (--i2clock_initd == 0) // only do it when the lock init is zero
     {
-        for (int i = 0; i < __I2CBUS_MAX_NUM; i++)
+        for (int i = 0; i < I2CBUS_MAX_NUM; i++)
         {
-            int ret = pthread_mutex_destroy(&(__i2cbus_locks[i]));
+            int ret = pthread_mutex_destroy(&(i2cbus_locks[i]));
             if (ret != 0)
             {
-                eprintf("%s: Failed to destroy mutex %d, ", __func__, i);
+                eprintf("Failed to destroy mutex %d, ", i);
                 perror("mutex destroy");
                 return -1;
             }
@@ -97,15 +139,10 @@ int i2cbus_close(i2cbus *dev)
     {
         if (dev->fd > 0)
             return close(dev->fd);
-        else
-        {
-            fprintf(stderr, "%s: %s %d\n", __func__, "Invalid file descriptor", dev->fd);
-            return -2;
-        }
     }
     else
     {
-        fprintf(stderr, "%s: %s\n", __func__, "Invalid device descriptor");
+        eprintf("Invalid device descriptor");
         return -3;
     }
     // will never reach this point
@@ -115,168 +152,169 @@ int i2cbus_close(i2cbus *dev)
 #define likely(x) __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
 
-int i2cbus_write(i2cbus *dev, const void *buf, ssize_t len)
+int i2cbus_write(i2cbus *dev, void *buf, int len)
 {
     // usual checks
     if (unlikely(dev == NULL || dev->fd < 0))
     {
-        fprintf(stderr, "%s: %s %p, %d\n", __func__, "Invalid device pointer or file descriptor", dev, dev->fd);
+        eprintf("Invalid device pointer %p or file descriptor %d", dev, dev->fd);
         return -1;
     }
-    // check if it belongs to a context
-    int locked = 1;
-    int status = 0;
-    if (!(dev->ctx < I2CBUS_CTX_0 || dev->ctx > I2CBUS_CTX_9))
+    if (unlikely(buf == NULL))
     {
-        status = pthread_mutex_trylock(dev->lock);
-        // valid context found, check if there is a lock
-        if (status == EBUSY || status == EINVAL)
-            locked = 0;
+        eprintf("Invalid write buffer pointer NULL");
+        return -1;
     }
-    // otherwise try to lock and block on it
-    else
-        status = pthread_mutex_lock(dev->lock);
-    if (status == EDEADLK) // we already own the mutex
-        locked = 0;
+    int status = pthread_mutex_lock(dev->lock);
+    if (status)
+    {
+        eprintf("Mutex lock returned %d, error", status);
+        return -1;
+    }
     status = write(dev->fd, buf, len);
     if (status != len)
     {
 #ifdef I2C_DEBUG
-        fprintf(stderr, "%s: Failed to write %ld bytes, wrote %d bytes\n", __func__, len, status);
+        eprintf("Failed to write %d bytes, wrote %d bytes, errno %d", len, status, errno);
 #endif
     }
-    if (locked)
-        pthread_mutex_unlock(dev->lock);
+    pthread_mutex_unlock(dev->lock);
     return status;
 }
 
-int i2cbus_read(i2cbus *dev, void *buf, ssize_t len)
+int i2cbus_read(i2cbus *dev, void *buf, int len)
 {
     // usual checks
     if (unlikely(dev == NULL || dev->fd < 0))
     {
-        fprintf(stderr, "%s: %s %p, %d\n", __func__, "Invalid device pointer or file descriptor", dev, dev->fd);
+        eprintf("Invalid device pointer %p or file descriptor %d", dev, dev->fd);
         return -1;
     }
-    // check if it belongs to a context
-    int locked = 1;
-    int status = 0;
-    if (!(dev->ctx < I2CBUS_CTX_0 || dev->ctx > I2CBUS_CTX_9))
+    if (unlikely(buf == NULL))
     {
-        status = pthread_mutex_trylock(dev->lock);
-        // valid context found, check if there is a lock
-        if (status == EBUSY || status == EINVAL)
-            locked = 0;
+        eprintf("Invalid write buffer pointer NULL");
+        return -1;
     }
-    // otherwise try to lock and block on it
-    else
-        status = pthread_mutex_lock(dev->lock);
-    if (status == EDEADLK) // we already own the mutex
-        locked = 0;
+    int status = pthread_mutex_lock(dev->lock);
+    if (status)
+    {
+        eprintf("Mutex lock returned %d, error", status);
+        return -1;
+    }
     status = read(dev->fd, buf, len);
     if (status != len)
     {
 #ifdef I2C_DEBUG
-        fprintf(stderr, "%s: Failed to read %ld bytes, read %d bytes\n", __func__, len, status);
+        eprintf("Failed to read %d bytes, read %d bytes, errno %d", len, status, errno);
 #endif
     }
-    if (locked)
-        pthread_mutex_unlock(dev->lock);
+    pthread_mutex_unlock(dev->lock);
     return status;
 }
 
 int i2cbus_xfer(i2cbus *dev,
-                void *outbuf, ssize_t outlen,
-                void *inbuf, ssize_t inlen,
+                void *outbuf, int outlen,
+                void *inbuf, int inlen,
                 unsigned long timeout_usec)
 {
     // usual checks
     if (unlikely(dev == NULL || dev->fd < 0))
     {
-        fprintf(stderr, "%s: %s %p, %d\n", __func__, "Invalid device pointer or file descriptor", dev, dev->fd);
+        eprintf("Invalid device pointer %p or file descriptor %d", dev, dev->fd);
         return -1;
     }
-    // check if it belongs to a context
-    int locked = 1;
-    int status = 0;
-    if (!(dev->ctx < I2CBUS_CTX_0 || dev->ctx > I2CBUS_CTX_9))
+    if (unlikely(outbuf == NULL))
     {
-        status = pthread_mutex_trylock(dev->lock);
-        // valid context found, check if there is a lock
-        if (status == EBUSY || status == EINVAL)
-            locked = 0;
-#ifdef I2C_DEBUG
-        eprintf("%s: Contex found, cts: 0x%02x, status: %d, locked: %d\n", __func__, dev->ctx, status, locked);
-#endif
+        eprintf("Invalid write buffer pointer NULL");
+        return -1;
     }
-    // otherwise try to lock and block on it
-    else
-        status = pthread_mutex_lock(dev->lock);
-    if (status == EDEADLK) // we already own the mutex
-        locked = 0;
+    if (unlikely(inbuf == NULL))
+    {
+        eprintf("Invalid read buffer pointer NULL");
+        return -1;
+    }
+    int status = pthread_mutex_lock(dev->lock);
+    if (status)
+    {
+        eprintf("Mutex lock returned %d, error", status);
+        return -1;
+    }
 #ifdef I2C_DEBUG
-    eprintf("%s: Sending %d bytes ->\n", __func__, outlen);
+    eprintf("Sending %d bytes ->", outlen);
     for (int i = 0; i < outlen; i++)
     {
-        eprintf("[0x%02x]", ((unsigned char *)outbuf)[i]);
+        fprintf(stderr, " %02xh", ((unsigned char *)outbuf)[i]);
     }
     eprintf("\n");
 #endif
     status = write(dev->fd, outbuf, outlen);
     if (status != outlen)
     {
-        fprintf(stderr, "%s: Failed to write %d bytes, wrote %d bytes\n", __func__, outlen, status);
-        goto i2cbus_xfer_end;
+#ifdef I2C_DEBUG
+        eprintf("Failed to write %d bytes, wrote %d bytes, errno %d", outlen, status, errno);
+#endif
+        goto ret;
     }
     if (timeout_usec > 0)
+    {
         usleep(timeout_usec);
+    }
     status = read(dev->fd, inbuf, inlen);
     if (status != inlen)
     {
 #ifdef I2C_DEBUG
-        fprintf(stderr, "%s: Failed to read %d bytes, read %d bytes\n", __func__, inlen, status);
+        eprintf("Failed to read %d bytes, read %d bytes, errno %d", inlen, status, errno);
 #endif
+        goto ret;
     }
 #ifdef I2C_DEBUG
-    eprintf("%s: Receiving %d bytes ->\n", __func__, inlen);
+    eprintf("Receiving %d bytes ->", inlen);
     for (int i = 0; i < inlen; i++)
     {
-        eprintf("[0x%02x]", ((unsigned char *)inbuf)[i]);
+        fprintf(stderr, " %02xh", ((unsigned char *)inbuf)[i]);
     }
     eprintf("\n");
 #endif
-i2cbus_xfer_end:
-    if (locked)
-        pthread_mutex_unlock(dev->lock);
+ret:
+    pthread_mutex_unlock(dev->lock);
     return status;
 }
 
-int i2cbus_lock(i2cbus *dev)
+int i2cbus_lock(unsigned int bus)
 {
-    if (likely(!(dev->ctx < I2CBUS_CTX_0 || dev->ctx > I2CBUS_CTX_9)))
+    if (unlikely(bus >= I2CBUS_MAX_NUM))
     {
-        return pthread_mutex_lock(dev->lock);
+        eprintf("Bus index %d not supported, maximum is %d", I2CBUS_MAX_NUM - 1);
+        return -100;
     }
-    else
-        return -EINVAL;
+    int ret = pthread_mutex_lock(&(i2cbus_locks[bus]));
+    if (ret)
+        return -ret;
+    return 1;
 }
 
-int i2cbus_trylock(i2cbus *dev)
+int i2cbus_trylock(unsigned int bus)
 {
-    if (likely(!(dev->ctx < I2CBUS_CTX_0 || dev->ctx > I2CBUS_CTX_9)))
+    if (unlikely(bus >= I2CBUS_MAX_NUM))
     {
-        return pthread_mutex_trylock(dev->lock);
+        eprintf("Bus index %d not supported, maximum is %d", I2CBUS_MAX_NUM - 1);
+        return -100;
     }
-    else
-        return -EINVAL;
+    int ret = pthread_mutex_trylock(&(i2cbus_locks[bus]));
+    if (ret)
+        return -ret;
+    return 1;
 }
 
-int i2cbus_unlock(i2cbus *dev)
+int i2cbus_unlock(unsigned int bus)
 {
-    if (likely(!(dev->ctx < I2CBUS_CTX_0 || dev->ctx > I2CBUS_CTX_9)))
+    if (unlikely(bus >= I2CBUS_MAX_NUM))
     {
-        return pthread_mutex_unlock(dev->lock);
+        eprintf("Bus index %d not supported, maximum is %d", I2CBUS_MAX_NUM - 1);
+        return -100;
     }
-    else
-        return -EINVAL;
+    int ret = pthread_mutex_unlock(&(i2cbus_locks[bus]));
+    if (ret)
+        return -ret;
+    return 1;
 }
--- i2cbus.h
+++ i2cbus.h
@@ -15,59 +15,20 @@ extern "C" {
 #endif
 #include <stdio.h>
 #include <pthread.h>
+
 /**
  * @brief Structure describing an I2C bus.
  * 
  */
-#ifdef __I2CBUS_INTERNAL
-#define __I2CBUS_MAX_NUM 2 /// Maximum 2 /dev/i2cX
-/**
- * @brief Set of mutexes for the I2C bus
- * 
- */
-pthread_mutex_t __i2cbus_locks[__I2CBUS_MAX_NUM];
-/**
- * @brief Set of the contexts for the lock
- * 
- */
-int __i2cbus_lock_ctx[__I2CBUS_MAX_NUM] = {
-    0,
-};
-#endif
-typedef enum
-{
-    I2CBUS_CTX_0 = 0xa,
-    I2CBUS_CTX_1,
-    I2CBUS_CTX_2,
-    I2CBUS_CTX_3,
-    I2CBUS_CTX_4,
-    I2CBUS_CTX_5,
-    I2CBUS_CTX_6,
-    I2CBUS_CTX_7,
-    I2CBUS_CTX_8,
-    I2CBUS_CTX_9,
-} i2cbus_ctxs;
-
 typedef struct
 {
     int fd;                /// I2C device file descriptor
     int id;                /// I2C device file id (X in /dev/i2cX)
     pthread_mutex_t *lock; /// Lock corresponding to the /dev/i2cX file, assigned from the locks array indexed by id
-    int ctx;               /// Context for the I2C device file.
-                           /// Multiple devices can share the same context if they are
-                           /// parts of the same module and require time-sensitive access to
-                           /// the i2c bus. In that case even if the mutex is locked for the
-                           /// bus, the system will proceed to provide read/write access for
-                           /// devices sharing the same context.
-                           /// Note: Context has to be set AFTER i2cbus_init() has been called.
 } i2cbus;
 /**
  * @brief Open an I2C bus file descriptor using the supplied parameters.
  * 
- * Note: This function does NOT set the context for the same block of
- * devices. Please set the ctx in dev manually as needed. Use the members
- * of the i2cbus_ctxs enum to populate the dev->ctx variable AFTER this 
- * function has been called.
  * 
  * @param dev i2c device descriptor
  * @param id i2c device file ID (X in /dev/i2cX)
@@ -78,85 +39,75 @@ int i2cbus_open(i2cbus *dev, int id, int addr);
 /**
  * @brief Close the file descriptor for the I2C device. 
  * 
- * @param dev i2c device descriptor. Note: Memory for dev is not freed by this function and it has to be freed by the allocator.
+ * @param dev i2c device descriptor.
  * @returns (int) Return code from close() or negative on error.
  */
 int i2cbus_close(i2cbus *dev);
 /**
  * @brief Write bytes to the i2c device.
- * 
- * Note: The write will proceed only when the corresponding mutex is
- * unlocked, OR if the device belongs to the same context in which
- * the bus was locked in the first place. In case of a pre-lock context,
- * this function WILL NOT unlock the mutex.
- * 
- * Note 2: It is logical to use read() or write() calls directly if
- * the execution is particularly time sensitive after acquiring a context
- * based buslock that can be retained between multiple read/write calls.
+ * Note: Bus access by this function is protected by a recursive
+ * pthread mutex.
  * 
  * @param dev i2c device descriptor
  * @param buf Pointer to byte array to write (MSB first)
  * @param len Length of byte array
  * @return int Length of bytes written on success, -1 on failure
  */
-int i2cbus_write(i2cbus *dev, const void *buf, ssize_t len);
+int i2cbus_write(i2cbus *dev, void *buf, int len);
 /**
- * @brief Read bytes from the i2c device
- * 
- * Note: The read will proceed only when the corresponding mutex is
- * unlocked, OR if the device belongs to the same context in which
- * the bus was locked in the first place. In case of a pre-lock context,
- * this function WILL NOT unlock the mutex.
+ * @brief Read bytes from the i2c device.
+ * Note: Bus access by this function is protected by a recursive
+ * pthread mutex.
  * 
  * @param dev i2c device descriptor
  * @param buf Pointer to byte array to read to (MSB first)
  * @param len Length of byte array
  * @return int Length of bytes read on success, -1 on failure
  */
-int i2cbus_read(i2cbus *dev, void *buf, ssize_t len);
+int i2cbus_read(i2cbus *dev, void *buf, int len);
 /**
  * @brief Function to do a write, and get the reply in one operation
  * in order to avoid read/write mangling with multiple threads 
  * accessing the bus. Avoid using this function if you have read or
  * write buffer lengths zero.
  * 
+ * Note: Bus access by this function is protected by a recursive
+ * pthread mutex.
+ * 
  * @param dev i2c device descriptor
  * @param outbuf Pointer to byte array to write (MSB first)
  * @param outlen Length of output byte array
- * @param inbuf Pointer to byte array to read to (MSB first)
+ * @param inbuf Pointer to byte array to read to (MSB first), can be the same as outbuf
  * @param inlen Length of input byte array
  * @param timeout_usec Timeout between read and write (in microseconds)
  * @return int Length of bytes read on success, -1 on failure
  */
 int i2cbus_xfer(i2cbus *dev,
-                void *outbuf, ssize_t outlen,
-                void *inbuf, ssize_t inlen,
+                void *outbuf, int outlen,
+                void *inbuf, int inlen,
                 unsigned long timeout_usec);
 /**
- * @brief Lock the mutex for the I2C bus ID. Use this only if you 
- * need continuous access to a bus for time sensitive reads. DO NOT
- * forget to call i2cbus_unlock() after this call.
+ * @brief Acquire lock on an i2c bus.
  * 
- * @param dev i2c device descriptor
- * @return int pthread_mutex_lock()
+ * @param bus Bus index (X in /dev/i2c-X)
+ * @return int Positive on success, negative on error (negative of error returned by pthread_mutex_lock)
  */
-int i2cbus_lock(i2cbus *dev);
+int i2cbus_lock(unsigned int bus);
 /**
- * @brief Try to lock the mutex for the I2C bus ID. Use this only if you 
- * need continuous access to a bus for time sensitive reads. DO NOT
- * forget to call i2cbus_unlock() after this call.
+ * @brief Try to acquire lock on an i2c bus,
+ * for timing jitter sensitive applications.
  * 
- * @param dev i2c device descriptor
- * @return int pthread_mutex_lock()
+ * @param bus Bus index (X in /dev/i2c-X)
+ * @return int Positive on success, negative on error (negative of error returned by pthread_mutex_trylock)
  */
-int i2cbus_trylock(i2cbus *dev);
+int i2cbus_trylock(unsigned int bus);
 /**
- * @brief Unlock the mutex for the I2C bus
+ * @brief Unlock an i2c bus.
  * 
- * @param dev i2c device descriptor
- * @return int pthread_mutex_unlock()
+ * @param bus Bus index (X in /dev/i2c-X)
+ * @return int int Positive on success, negative on error (negative of error returned by pthread_mutex_lock)
  */
-int i2cbus_unlock(i2cbus *dev);
+int i2cbus_unlock(unsigned int bus);
 #ifdef __cplusplus 
 }
 #endif
